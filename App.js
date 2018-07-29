1171188d6c7785497b94ff394566097fd6d4d431
daniel-certa-1228 <mfdancerta@gmail.com>
2018-07-28 20:20:36 -0500

    pizza translator

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
-import { AppRegistry, View } from 'react-native';
+import { AppRegistry, Text, TextInput, View } from 'react-native';
 
-export default class FixedDimensionsBasics extends Component {
+export default class PizzaTranslator extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {text: ''};
+    }
+
     render() {
         return (
-                // Try removing the `flex: 1` on the parent View.
-                // The parent will not have dimensions, so the children can't expand.
-                // What if you add `height: 300` instead of `flex: 1`?
-            <View style={{
-                    flex: 1, 
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'center'
-                }}>
-                <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-                <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-                <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+            <View style={{padding: 10, marginTop: 20}}>
+                <TextInput
+                style={{height:40}}
+                placeholder="Type here to translate!"
+                onChangeText={(text) => this.setState({text})}
+                />
+                <Text style={{padding: 10, fontSize:42}}>
+                    {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+                </Text>
             </View>
-        )
+        );
     }
 }
